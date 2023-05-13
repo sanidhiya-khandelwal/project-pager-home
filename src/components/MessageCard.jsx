0bd85016e4d59e0a7adcf47b5f0f6b2fc765c019
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-05-13 18:42:24 +0530

    seventh commit

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -4,8 +4,8 @@ import './MessageCard.css'
 const MessageCard = ({ anything, index }) => {
     return (
         <div className='message-card-container'>
-            <p className='username'>{anything.userName}</p>
-            <p className='usermesssage'>{anything.userMessage}</p>
+            <p className='username'>Sender Name : {anything.userName}</p>
+            <p className='usermesssage'>Message : {anything.userMessage}</p>
             <p className="date">{anything.userDate}</p>
         </div>
     )
